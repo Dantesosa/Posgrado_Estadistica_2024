655673b12a136fb420207002e8dd3bce2ca4ad87
Dante Posgrado <dantesosa2@gmail.com>
2024-09-04 12:07:53 -0600

    Actualización de pruebas t en la clase 3

--- Tareas/Basico_1.R
+++ Tareas/Basico_1.R
@@ -1,3 +1,21 @@
 # Dante Siboldi Sosa Álvarez 
 # 1843694
 # 14/08/24
+
+
+# Primeros datos  ---------------------------------------------------------
+
+4+3
+5*6
+
+altura <- c(6,8,2,9,7,4)
+altura
+
+altura*5
+
+# Interpretar los resultados 
+
+
+# Segundos datos ----------------------------------------------------------
+
+
---
+++ Tareas/Lab_Sem3_DanteSosa.R
@@ -0,0 +1,91 @@
+prof_url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv"
+profepa<- read.csv(prof_url, encoding = "latin1")
+head(profepa)
+
+
+# El enconding = "latin1" es para detectar acentos en Español
+
+mean(profepa$Inspección)  
+
+#Mean se refiere a la media, $ significa la selección de la variables
+
+
+summary(profepa)
+
+
+ins <-subset(profepa, profepa$Inspección >= mean(profepa$Inspección))
+
+
+# codigo subset es una función para hacer un subconjunto de un conjunto de datos
+
+bajo <- subset(profepa, profepa$Inspección <= mean(profepa$Inspección))
+
+
+ceroins <- subset(profepa,profepa$Inspección == 0)
+
+
+# Descarga de datos seguro
+
+library(repmis)
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+
+conjunto$Especie <- as.factor(conjunto$Especie)
+conjunto$Clase <- as.factor(conjunto$Clase)
+
+# as.facor es el código para hacer que las caracteres se vuelvan tratamientos
+
+
+summary(conjunto)
+
+
+SpFH <- subset(conjunto,conjunto$Especie !="C")
+tapply(SpFH$Diametro, SpFH$Especie, mean)
+
+boxplot(SpFH$Diametro ~ SpFH$Especie,
+        xlab = "Especies", 
+        ylab = "Diámetro",
+        col= "gray")
+
+#Prueba de Normalidad 
+
+shapiro.test(SpFH$Diametro)
+
+
+
+
+
+#Prueba de homogeneidad
+
+bartlett.test(SpFH$Diametro, SpFH$Especie)
+
+
+
+
+#Prueba de t independientes
+
+t.test(SpFH$Diametro ~ SpFH$Especie, var.equal = TRUE)
+
+
+
+#Prueba t de una muestra 
+
+t.test(conjunto$Diametro, mu = 16.4)
+
+
+#el codigo mu = para aclarar que es una prueba de una muestra 
+
+
+
+
+# Prueba para muestreas dependientes
+
+
+
+prod <- read.csv("produccion.csv", header=T)
+
+
+
+
+
+
+
